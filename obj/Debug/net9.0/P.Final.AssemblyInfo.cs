8b200ada943fb6718a0c8bd708bf5d7309592073
CrisGLpez <maximuselectris@gmail.com>
2025-04-03 18:31:52 -0400

    Modulo Agente Añadido

--- obj/Debug/net9.0/P.Final.AssemblyInfo.cs
+++ obj/Debug/net9.0/P.Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P.Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50d06862a6a6b7ac57157f0a85cc7bf5efc984c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("P.Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P.Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
